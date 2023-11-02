2cf02bf16fbfb04529bd943c5e92ea832c8fedc3
Leo Phan-cao <leophancaowz@gmail.com>
2023-11-03 00:25:29 +0100

    [ADD]: Error Handling (server connection & disconnection)

--- world/world.gd
+++ world/world.gd
@@ -12,19 +12,35 @@ func _unhandled_input(event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 
+func _notification(event):
+	if event == NOTIFICATION_WM_CLOSE_REQUEST:
+		multiplayer.multiplayer_peer = null
+		get_tree().quit()
+
 func _on_host_pressed():
+	# Start as server.
 	main_menu.hide()
 	
 	enet_peer.create_server(PORT)
+	if enet_peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer server.")
+		return
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
 	
 	add_player(multiplayer.get_unique_id())
 
 func _on_join_pressed():
 	main_menu.hide()
-	
+	if address_entry.text == "":
+		OS.alert("Need a remote to connect to.")
+		return
+
 	enet_peer.create_client(address_entry.text, PORT)
+	if enet_peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to start multiplayer client.")
+		return
 	multiplayer.multiplayer_peer = enet_peer
 
 func add_player(peer_id):
@@ -36,4 +52,3 @@ func remove_player(peer_id):
 	var player = get_node_or_null(str(peer_id))
 	if player:
 		player.queue_free()
-
